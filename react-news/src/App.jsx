f71218887bdc3143d1e83a21d0c2bb51284d17f2
dondifer <dondifer@gmail.com>
2023-06-14 20:10:59 +0200

    feat: styles and footer created

--- react-news/src/App.jsx
+++ react-news/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./components/Home/Home";
 import Header from "./components/Header/Header.jsx";
 import Form from "./components/Form/Form.jsx";
 import List from "./components/List/List.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
             <Route path="/form" element={<Form />} />
             <Route path="/list" element={<List />} />
           </Routes>
+          <Footer />
         </BrowserRouter>
       </div>
     </>
